79c15026599699eccfc0cf3b0399411311dd8f21
Salsabeel <58127260+sals1995@users.noreply.github.com>
2025-06-03 12:53:23 +0300

    lab is added

---
+++ src/tests/lab/lab.test.jsx
@@ -0,0 +1,10 @@
+
+describe.skip('formatPrice function', () => {
+  it('formats number with default currency and 2 decimals', () => { });
+  it('formats number with custom currency symbol and decimals', () => {});
+  it('returns empty string for invalid input', () => {});
+})
+describe.skip('HeroesFromAPI component', () => {
+  it('renders "No heroes available" when API returns empty list',()=>{});
+  it('renders heroes fetched from API', () => {});
+});
