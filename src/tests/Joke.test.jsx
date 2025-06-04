9f05c54483776ff38696946a37389fe94aebd172
Salsabeel <58127260+sals1995@users.noreply.github.com>
2025-06-04 12:19:20 +0300

    lab is added

--- src/tests/Joke.test.jsx
+++ src/tests/Joke.test.jsx
@@ -2,6 +2,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 import JokeFetcher from "../components/Joke/Joke";
 import { afterAll, beforeAll } from "vitest";
 import server from "../mocks/server";
+import { http } from "msw";
 
 describe("Joke component", () => {
   beforeAll(() => {
@@ -23,5 +24,20 @@ describe("Joke component", () => {
     let h1 = await screen.findByRole("heading");
     expect(h1).toHaveTextContent(/funny joke/i);
   });
- 
+  it("render with response error", async () => {
+    //before rendering component 
+    //override the request handler in server.js to make response with error
+    server.use(
+        http.get("https://api.chucknorris.io/jokes/random",()=>{
+        return HttpResponse.json({},{status:500})
+    }))
+    render(<JokeFetcher />);
+
+    expect(screen.getByRole("heading")).toHaveTextContent(/loading/i);
+    
+    
+    //then check if error is rendered
+    let h1 = await screen.findByRole("heading");
+    expect(h1).toHaveTextContent(/failed/i);
+  });
 });
---
+++ src/tests/lab/lab.test.jsx
@@ -0,0 +1,11 @@
+
+describe.skip('formatPrice function', () => {
+  it('formats number with default currency and 2 decimals', () => { });
+  it('formats number with custom currency symbol and decimals', () => {});
+  it('returns empty string for invalid input', () => {});
+})
+describe.skip('HeroesFromAPI component', () => {
+  it('renders "No heroes available" when API returns empty list',()=>{});
+  it('renders heroes fetched from API', () => {});
+  it('renders error message when API fails',  () => {});
+});
